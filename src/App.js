f0fe967b1f907881786203a5ad4fc34a2cfe078e
Aijaz_Bhat123 <aijaz.ahamed@reachiq.ai>
2024-08-29 15:54:43 +0530

    gym-guys page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,470 @@
-import logo from './logo.svg';
-import './App.css';
 
+import logo from "./assets/back.png"
+import gym from "./assets/gym.png"
+import txtImg from "./assets/txt.png"
+import pic from "./assets/Rectangle 1.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={
+      {
+        display: "flex",
+        flexDirection: "column",
+        gap: "32px",
+      }
+    }>
+      <div
+        style={
+          {
+            background: `url(${logo})`,
+            height: "456px",
+          }
+        }>
+        <div style={
+          {
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "18px 60px 0px 60px",
+          }
+        }>
+          <div style=
+            {{
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "600",
+              color: "white",
+              fontSize: "16px"
+            }}>
+            Summary Report
+          </div>
+          <div>
+            <div>
+              <img src={gym} alt="gym-guyz" />
+            </div>
+            <div>
+              <img src={txtImg} alt="gym-txt" />
+            </div>
+          </div>
+        </div>
+        <div style={
+          {
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0px 60px 0px 60px",
+          }
+        }>
+          <div style={
+            {
+              marginTop: "40px"
+            }
+          }>
+            <div style=
+              {{
+                fontFamily: '"Oswald", sans-serif',
+                fontWeight: "700",
+                color: "white",
+                fontSize: "54px"
+              }}>
+              A Comprehensive
+            </div>
+            <div style=
+              {{
+                fontFamily: '"Oswald", sans-serif',
+                fontWeight: "700",
+                color: "white",
+                fontSize: "54px"
+              }}>
+              Physical Assessment Report
+            </div>
+            <div style={
+              {
+                borderTop: "2.82px solid #E4002B",
+                marginTop: "11.3px"
+              }
+            }>
+
+            </div>
+            <div style={
+              {
+                display: "flex",
+                gap: "48px",
+                marginTop: "11.3px"
+              }
+            }>
+              <div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "400",
+                    color: "white",
+                    fontSize: "16px"
+                  }
+                }>
+                  Assessment ID: #145668
+                </div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "500",
+                    color: "white",
+                    fontSize: "28px"
+                  }
+                }>
+                  Brooklyn Simmons
+                </div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "400",
+                    color: "white",
+                    fontSize: "16px"
+                  }
+                }>
+                  Male | 29 years
+                </div>
+              </div>
+              <div style={
+                {
+                  borderLeft: "1px solid white"
+                }
+              }>
+                |
+              </div>
+              <div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "400",
+                    color: "white",
+                    fontSize: "16px"
+                  }
+                }>
+                  Trainer ID: #145668
+                </div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "500",
+                    color: "white",
+                    fontSize: "28px"
+                  }
+                }>
+                  Jenny Wilson
+                </div>
+                <div style={
+                  {
+                    fontFamily: '"Oswald", sans-serif',
+                    fontWeight: "400",
+                    color: "white",
+                    fontSize: "16px"
+                  }
+                }>
+                  Male | 29 years
+                </div>
+              </div>
+              <div>
+              </div>
+            </div>
+          </div>
+          <div style={
+            {
+              marginTop: "3px"
+            }
+          }>
+            <img src={pic} alt="pic" />
+          </div>
+        </div>
+      </div>
+      <div style={
+        {
+          margin: "0px 60px"
+        }
+      }>
+        <div style={
+          {
+            // height:"43px",
+            backgroundColor: "#F4F5EF",
+            padding: "8px 16px",
+            fontFamily: '"Oswald", sans-serif',
+            fontWeight: "500",
+            color: "#000000",
+            fontSize: "18px"
+
+          }
+        }>
+          Mindbody Data Validation
+        </div>
+      </div>
+      <div style={
+        {
+          margin: "0px 60px",
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "10px",
+          padding: "10px",
+        }
+      }>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            First Name
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Brookyln
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Last Name
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Simmons
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            City
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Scottsdale
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Address Line
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            15205 North Kierland Blvd, Suite 100
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            State
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Arizona
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Zip Code
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            85254
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Phone Number
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            +1-6154865695
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Email Address
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Brooklynsimmons@gmail.com
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Date of Birth
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            12-01-1998
+          </div>
+        </div>
+        <div style={
+          {
+            display:"flex",
+            flexDirection:"column",
+            gap:"8px"
+          }
+        }>
+          <div style={
+            {
+              fontFamily: '"Oswald", sans-serif',
+              fontWeight: "400",
+              color: "#000000",
+              fontSize: "16px"
+            }
+          }>
+            Gender
+          </div>
+          <div style={{
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: "400",
+            color: "#666666",
+            fontSize: "16px"
+          }}>
+            Male
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
